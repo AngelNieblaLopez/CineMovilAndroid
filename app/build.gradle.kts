b9ba41a4582ea0d6102846cbb2518de52aa9747b
ANGELDANIELORTIZ <ad.ortiz20@info.uas.edu.mx>
2023-11-18 00:54:36 -0700

    magic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,13 +3,18 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cinemovil"
+
+    lintOptions{
+        isCheckReleaseBuilds = false
+    }
+
+    namespace = "com.example.magic"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.cinemovil"
-        minSdk = 24
-        targetSdk = 33
+        applicationId = "com.example.magic"
+        minSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +38,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/magic/Activities/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.magic.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.magic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button getInBtn=findViewById(R.id.getInBtn);
+        getInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, login.class));
+            }
+        });
+    }
+}
