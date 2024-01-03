19994fb22293695b71b8a9c1064895bb48920a8e
Angel <ad.niebla20@info.uas.edu.mx>
2024-01-02 23:41:15 -0700

    detalle wip

--- app/src/main/java/com/example/magic/retrofit/movies/Movie.java
+++ app/src/main/java/com/example/magic/retrofit/movies/Movie.java
@@ -1,11 +1,18 @@
 package com.example.magic.retrofit.movies;
 
 import com.example.magic.Domain.ListFilm;
+import com.example.magic.retrofit.functions.ApiResponseScheduleFunctions;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface Movie {
     @GET("movies/v1/with_function")
     Call<ListFilm> withFunction();
+
+    @GET("movies/v1/{id}")
+    Call<ApiResponseMovieDetail> detail(
+            @Path("id") int id
+    );
 }
